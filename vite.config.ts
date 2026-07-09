2ec58be294ff13681cf7b6a462341c29a36d3257
nstcrystal <nstcrystal@gmail.com>
2026-07-09 20:57:25 +0700

    feat: update base URL in Vite configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
+  base: '/ten-repository-cua-ban/',
   plugins: [
     vue(),
     vueDevTools(),
